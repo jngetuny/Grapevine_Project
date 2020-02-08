cc55cf189d282f1ff7fab34087cffd7b5cf735b8
John Ngetuny <53797847+jngetuny@users.noreply.github.com>
2020-02-07 18:36:51 -0600

    Add files via upload

---
+++ XYLab2.py
@@ -0,0 +1,6 @@
+R =int(input('Enter the length of the row, in feet.:'))
+E=int(input('Enter the amount of space, in feet, used by an end-post assembly.:')) 
+S=int(input('Enter the space between vines, in feet.:'))
+V1=R-2*4
+V=V1/2
+print(V)
